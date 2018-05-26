988d61e7cec4de67e084b9b87a8172e0c15db479
Zoe <jsz3@live.com>
2018-05-26 20:07:12 +0800

    fix attr

--- js/g.c.js
+++ js/g.c.js
@@ -322,20 +322,24 @@ function(e, t, i) {
                 wx.checkJsApi({
                     jsApiList: ["getLocation"],
                     success: function(res) {
-                        if (res.checkResult.getLocation == false) {
-                            alert('你的微信版本太低，不支持微信JS接口，请升级到最新的微信版本！');
-                            return;
-                        }
-                        wx.getLocation({
+                        var p = document.createElement("p")
+                        p.innterText = JSON.stringify(res)
+                        document.body.append(p)
+                        res.checkResult.getLocation && wx.getLocation({
                             success: function (res) {
                                 var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
                                 var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
                                 var speed = res.speed; // 速度，以米/每秒计
                                 var accuracy = res.accuracy; // 位置精度
-                                alert(JSON.stringify(res))
+
+                                var p = document.createElement("p")
+                                p.innterText = JSON.stringify(res)
+                                document.body.append(p)
                             },
                             cancel: function (res) {
-                                alert('用户拒绝授权获取地理位置');
+                                var p = document.createElement("p")
+                                p.innterText = '用户拒绝授权获取地理位置'
+                                document.body.append(p)
                             }
                         });
                     }
